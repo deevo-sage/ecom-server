a1da6d733c8defea9587969cc1c16b86f0376552
deevo-sage <sidharthsahniii@gmail.com>
2021-04-09 20:24:36 +0530

    Revert "added more controllers"
    This reverts commit c48ec0be6f656bac7c3c5ed58ec149079f5cdb96.

--- config.js
+++
@@ -1,6 +0,0 @@
-let config = {
-  port: 8000,
-  db:
-    "mongodb+srv://user:user123@cluster0.5g25f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-};
-export default config;
--- models/Seller.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const Seller = new mongoose.Schema({
-  userId: String, 
-  gst: Number,
-  items:[{id:String}],
-  ordersRecieved:[{id:String}]
-});
-
-export default mongoose.model("Seller", Seller);
